193dcc7a3d167bce1aca7fe4c01aad48439f03d7
Florian Bruggisser <florian@bildspur.ch>
2018-11-23 14:14:44 +0100

    fixed with own map function

--- src/driver/motor/PreciseServo.cpp
+++ src/driver/motor/PreciseServo.cpp
@@ -36,7 +36,11 @@ void PreciseServo::movePrecise(float angle, bool delayForServo) {
     if (delayForServo) {
         // calculate delta
         auto deltaPulse = abs(pulseBefore - pulse);
-        auto deltaDegree = MathUtils::limit(MathUtils::map(deltaPulse, SERVO_MIN_PULSE, SERVO_MAX_PULSE, 0.0f, 180.0f), 0.0f, 180.0f);
+
+        // MathUtils::map(deltaPulse, SERVO_MIN_PULSE, SERVO_MAX_PULSE, 0.0f, 180.0f)
+        auto deltaDegree = MathUtils::limit(
+                static_cast<float>(deltaPulse) / (SERVO_MAX_PULSE - SERVO_MIN_PULSE) * 180.0f,
+                0.0f, 180.0f);
         auto millisToWait = std::lround(servoSpeedPerDegree * deltaDegree * 1000);
 
         Serial.printf("Angle: %f Delta Pulse: %i, Delta Degree: %f, Millis: %lu\n", angle, deltaPulse,
